82b03e91996b033f54ced4c8d252c4a4170a9eeb
lucianoontiveros <luciano.a.ontiveros@gmail.com>
2023-01-16 13:08:12 -0300

    Se agrega validacion de formulario de registro

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,43 +1,71 @@
+import { Formik } from "formik";
 import { useState } from "react";
 import { register } from "../config/firebase";
 import { useRedirecActiveUser } from "../hooks/useRedirectActiveUser";
-
+import * as Yup from 'yup'
 const Register = () => {
 
-    const [ email, setEmail] = useState("");
-    const [ password, setPassword] = useState("");
     useRedirecActiveUser('/dashboard')
 
-    const handleSubmit = async(e) => {
-        e.preventDefault()
-        
-        try{
-           const credentialUser =  await register({email, password})
-            console.log(credentialUser)
+    const onsubmit = async ({ email, password}, { setSubmitting, setErrors, resetForm}) => {
+        try {
+            const registerUser = await register({email, password})
+            console.log(registerUser)
+            resetForm()
         } catch (error) {
-            console.log(error)
+            if(error.code === 'auth/email-already-in-use') {
+                return setErrors({ email: "El usuario ya esta registrado"})
+            }
+        } finally {
+            setSubmitting(false)
         }
     }
+
+    const validationSchema = Yup.object().shape({
+        email: Yup.string().email("Ingresar un correo valido").required("requiere un correo electronico"),
+        password: Yup.string().trim().min(6, "minimo seis caracteres").required('Password requerido'),
+      });
+    
 
     return(
         <>
             <h1>Register</h1>
-            <form onSubmit={handleSubmit}>
-                <input 
-                    type="text" 
-                    placeholder="Ingrese email"
-                    value={email}
-                    onChange={ (e) => setEmail(e.target.value)}
-                />
-                <input 
-                    type="password" 
-                    value={password}
-                    placeholder="Ingrese Contraseña"
-                    onChange={ (e) => setPassword(e.target.value)}
+            <Formik
+                initialValues={{email: '', password: ''}}
+                onSubmit={onsubmit}
+                validationSchema={validationSchema}
+            >
+                {
+                    ({ values, handleSubmit, handleChange, errors, touched, handleBlur, isSubmitting  }) => (
+                        <form onSubmit={handleSubmit}>
+                            <input 
+                                type="text" 
+                                placeholder="Ingrese email"
+                                value={values.email}
+                                name='email'
+                                onBlur={handleBlur}
+                                onChange={handleChange}
+                            />
+                            {
+                                errors.email && touched.email && errors.email
+                            }
+                            <input 
+                                type="password" 
+                                value={values.password}
+                                placeholder="Ingrese Contraseña"
+                                name="password"
+                                onBlur={handleBlur}
+                                onChange={handleChange}
 
-                />
-                <button>Register</button>
-            </form>
+                            />
+                            {
+                                errors.password && touched.password && errors.password
+                            }
+                            <button type="submit" disabled={isSubmitting }>Register</button>
+                        </form>
+                    )
+                }
+            </Formik>
         </>
     )
 }
